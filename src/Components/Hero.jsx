3141df4547f31fc617e1789e9292660b046c4d8a
MANISH <khandagalemanish21@gmail.com>
2024-12-18 20:15:06 +0530

    first commit

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='relative h-dvh w-screen overflow-x-hidden'>
+        <div id='video-frame' className='relative overflow-hidden rounded-lg z-10 bg-blue-75 h-dvh w-screen' >
+<div>
+    <div className='mask-clip-path absolute absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
+<div>
+    minivp
+</div>
+    </div>
+</div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
