91e7e5f2e922c4d4737139217fcaee12238935e3
MANISH <khandagalemanish21@gmail.com>
2024-12-20 13:31:54 +0530

    animation added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import {ScrollTrigger} from 'gsap/all'
+import AnimatedTitle from './AnimatedTitle'
 
+gsap.registerPlugin(ScrollTrigger)
 const About = () => {
+
+useGSAP(()=>{
+  const ClipAnimation = gsap.timeline({
+    scrollTrigger:{
+      trigger: "#clip",
+      start:"center center",
+      end:"+800 center",
+      scrub:0.5,
+      pin:true,
+      pinSpacing:true,
+    }
+  })
+  ClipAnimation.to('#mask-clip-path',{
+width:'100vw',
+height:'100vh',
+borderRadius: 0 ,
+  })
+})
+
   return (
     <div id='about' className='min-h-screen w-screen '>
         <div className='relative flex flex-col mb-8 mt-36 items-center gap-5'>
             <h2 className='font-general text-sm uppercase md:text-[10px]'>Welcome To  Zentry</h2>
-            <div className='text-4xl font-zentry mt-5 text-center uppercase leading-[0.8] md:text-[6rem] '>
-            Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure
-            </div>
+            <AnimatedTitle title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure" containerclass="mt-5 !text-black text-center"/>
+          
             <div className='about-subtext'>
             <p>The Game of Games begins—your life, now an epic MMORPG</p>
           <p className="text-gray-500">
@@ -17,7 +40,7 @@ const About = () => {
             </div>
         </div>
         <div id='clip' className='h-dvh w-screen'>
-            <div className='about-image mask-clip-path'>
+            <div id='mask-clip-path' className='about-image'>
 <img src="img/about.webp" alt="background" className='absolute top-0 left-0 size-full object-cover' />
             </div>
         </div>
