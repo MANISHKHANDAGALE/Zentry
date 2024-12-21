147a48dc5a1444c7b3a33bac9e6f4114b5419685
MANISH <khandagalemanish21@gmail.com>
2024-12-21 16:31:03 +0530

    deploy

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,7 +9,7 @@ const socialLinks = [
 
 const Footer = () => {
   return (
-    <footer className="w-screen bg-[#5542ff] py-4 text-black">
+    <footer className="w-screen bg-white py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
           ©MANISH KHANDAGALE 2025. All rights reserved
@@ -28,10 +28,9 @@ const Footer = () => {
             </a>
           ))}
         </div>
-
         <a
           href="#privacy-policy"
-          className="text-center text-sm font-light hover:underline md:text-right"
+          className="text-center text-sm font-light hover:underline md:text-right md:mr-3"
         >
           Privacy Policy
         </a>
